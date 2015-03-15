4116cccbe97c24802faeab5263d0aa79f012ed0f
Randall Britten <randall.britten@gmail.com>
2015-03-15 21:28:48 +1300

    Pruned out most of the remaining "Mario" code.

--- StopStrep.elm
+++ StopStrep.elm
@@ -16,19 +16,13 @@ strepStartY = 20
 -- MODEL
 
 
-world = { x=0, y=0, vx=0, vy=0, dir="right", strepY=strepStartY, zapped= False }
+world = { x=0, y=0, strepY=strepStartY, zapped= False }
 
 
--- UPDATE -- ("m" is for World)
-jump {y} m = if y > 0 && m.y == 0 then { m | vy <- 5 } else m
-gravity t m = if m.y > 0 then { m | vy <- m.vy - t/16 } else m
-physics t m = { m | x <- m.x + t*m.vx , y <- max 0 (m.y + t*m.vy) }
-walk {x} m = { m | vx <- toFloat x
-                 , dir <- if x < 0 then "left" else
-                          if x > 0 then "right" else m.dir }
-scroll t m = if m.strepY > strepMaxY then { m | strepY <- strepStartY, zapped <- False } else {m | strepY <- m.strepY + t}
-zap touch m = Maybe.withDefault m ( Maybe.map (zap2 m) touch)
-zap2 m (x,y) = if (close 0 x) && (close (round m.strepY) y) then {m | zapped <- True} else m
+-- UPDATE -- ("w" is for World)
+scroll t w = if w.strepY > strepMaxY then { w | strepY <- strepStartY, zapped <- False } else {w | strepY <- w.strepY + t}
+zap touch w = Maybe.withDefault w ( Maybe.map (zap2 w) touch)
+zap2 w (x,y) = if (close 0 x) && (close (round w.strepY) y) then {w | zapped <- True} else w
 
 tolerance : Int
 tolerance = 40
@@ -40,15 +34,12 @@ step (dt, keys, touches, (w,h)) =
   let
     touch = if | List.isEmpty touches -> Nothing
                | otherwise -> Just ((List.head touches).x - round ((toFloat w)/2), round ((toFloat h)/2) - (List.head touches).y  )
-  in jump keys >> gravity dt >> walk keys >> physics dt >> scroll dt >> zap touch
+  in scroll dt >> zap touch
 
 
 -- DISPLAY
 render (w',h') world =
   let (w,h) = (toFloat w', toFloat h')
-      verb = if | world.y  >  0 -> "jump"
-                | world.vx /= 0 -> "walk"
-                | otherwise     -> "stand"
       src = "media/Injection.png"
   in collage w' h'
       [ rect w h  |> filled (rgb 174 238 238)
